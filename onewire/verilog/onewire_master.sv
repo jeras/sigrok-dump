7edb2c68b90f911cb150873c5ac610a3a63336f2
Iztok Jeras <iztok.jeras@gmail.com>
2012-05-12 21:21:19 +0200

    the master was rewritten, some waveoforms are now created

--- onewire/verilog/onewire_master.sv
+++ onewire/verilog/onewire_master.sv
@@ -24,92 +24,70 @@
 `timescale 1us / 1ns
 
 module onewire_master #(
-    parameter TS = 30.0
+    // optimal timig for master
+    parameter T_RSTL = 480.0,  // reset low (min)
+    parameter T_RSTH = 480.0,  // reset high (min)
+    parameter T_RSTP =  75.0,  // reset presence detect (max)
+    parameter T_DAT0 =  60.0,  // data bit 0 (min)
+    parameter T_DAT1 =   5.0,  // data bit 1 (min)
+    parameter T_DATS =  15.0,  // data sample (max)
+    parameter T_REC  =   5.0   // recovery (min)
 //  real TS = 30.0   // time slot (min=15.0, typ=30.0, max=60.0)
 )(
-  inout logic owr  // 1-wire
+  inout wire owr  // 1-wire
 );
 
-// configuration
-logic ena;    // response enable
-logic ovd;    // overdrive mode select
-logic dat_r;  // read data
-logic dat_w;  // write data
+logic pull = '0;
 
-// IO
-logic pul;
-logic dat;
-
-// events
-event sample_dat;
-event sample_rst;
+assign owr = pull ? '0 : 'z;
 
 //////////////////////////////////////////////////////////////////////////////
-// IO
+// IO tasks
 //////////////////////////////////////////////////////////////////////////////
 
-// onewire open collector signal
-assign owr = pul & ena ? 1'b0 : 1'bz;
-
-// read data output
-assign dat_w = ena ? dat : 1'bz;
-
-//////////////////////////////////////////////////////////////////////////////
-// events inside a cycle
-//////////////////////////////////////////////////////////////////////////////
-
-// power up state
-initial pul  <= 1'b0;
-
-always @ (negedge owr)  if (ena)  transfer (ovd, dat_r, dat);
-
-task automatic transfer (
-  input  ovd,
-  input  dat_r,
-  output dat_w
+task onewire_reset (
+  output logic presence
 ); begin
-  // provide read data response
-  pul = ~dat_r;
-  // wait 1 time slot
-  if (ovd)  #(1*TS/8);
-  else      #(1*TS);
-  // write data is sampled here
-  -> sample_dat;
-  dat_w = owr;
-  // release the wire
-  pul = 1'b0;
-  // fork into data or reset cycle
-  fork
-    // transfer data
-    begin : transfer_dat
-      // if cycle ends before reset is detected
-      if (~owr) @ (posedge owr);
-      // disable reset path
-      disable transfer_rst;
-    end
-    // transfer reset
-    begin : transfer_rst
-      // wait 7 time slots
-      if (ovd)  #(7*TS/8);
-      else      #(7*TS);
-      // reset is sampled here
-      -> sample_rst;
-      // if reset is detected disable data path
-      if (~owr) disable transfer_dat;
-      // wait for reset low to end
-      @ (posedge owr)
-      // wait 1 time slot
-      if (ovd)  #(1*TS/8);
-      else      #(1*TS);
-      // provide presence pulse
-      pul = 1'b1;
-      // wait 4 time slot
-      if (ovd)  #(4*TS/8);
-      else      #(4*TS);
-      // release the wire
-      pul = 1'b0;
-    end
-  join
-end endtask
+  // low
+  pull = '1;
+  # T_RSTL;
+  // high
+  pull = '0;
+  # T_RSTP;
+  // presence detect
+  presence = owr;
+  # (T_RSTH-T_RSTP);
+end
+endtask
+
+task onewire_bit (
+  input  bit   data_w,
+  output logic data_r
+); begin
+  // low
+  pull = '1;
+  # T_DAT1;
+  // send 0 or 1
+  pull = data_w ? '0 : '1;
+  # (T_DAT1-T_DATS);
+  // data sample
+  data_r = owr;
+  // tail
+  # (T_DAT0-T_DAT1-T_DATS);
+  pull = '0;
+  // recovery
+  # T_REC;
+end
+endtask
+
+task onewire_byte (
+  input  bit   [7:0] data_w,
+  output logic [7:0] data_r
+);
+  int i;
+begin
+  for (i=0; i<8; i++)  onewire_bit (data_w[i], data_r[i]);
+end
+endtask
 
 endmodule
--- onewire/verilog/onewire_tb.sv
+++ onewire/verilog/onewire_tb.sv
@@ -30,22 +30,15 @@ localparam OWN = 1;    // slaves with different timing (min, typ, max)
 // onewire
 wire [OWN-1:0] owr;     // bidirectional
 
-// slave conviguration
-reg            slave_ena;    // slave enable (connect/disconnect from wire)
-reg      [3:0] slave_sel;    // 1-wire slave select
-reg            slave_ovd;    // overdrive mode enable
-reg            slave_dat_r;  // read  data
-wire [OWN-1:0] slave_dat_w;  // write data
-
-// error checking
-integer        error;
-integer        n;
-
 // overdrive enable loop
 integer        i;
 
+logic       presence;
+logic [7:0] data_w;
+logic [7:0] data_r;
+
 //////////////////////////////////////////////////////////////////////////////
-// configuration printout and waveforms
+// VCD waveforms
 //////////////////////////////////////////////////////////////////////////////
 
 // request for a dumpfile
@@ -55,148 +48,26 @@ initial begin
 end
 
 //////////////////////////////////////////////////////////////////////////////
-// Avalon write and read transfers
+// program
 //////////////////////////////////////////////////////////////////////////////
 
-/*
 initial begin
-  // reset error counter
-  error = 0;
-
-  // long delay to skip presence pulse
-  slave_ena = 1'b0;
-  #1000_000;
-
-  // test with slaves with different timing (each slave one one of the wires)
-  for (slave_sel=0; slave_sel<OWN; slave_sel=slave_sel+1) begin
-
-    // select normal/overdrive mode
-    //for (slave_ovd=0; slave_ovd<(OVD_E?2:1); slave_ovd=slave_ovd+1) begin
-    for (i=0; i<(OVD_E?2:1); i=i+1) begin
-
-      slave_ovd = i[0];
-
-      // generate a reset pulse
-      slave_ena   = 1'b0;
-      slave_dat_r = 1'b1;
-      avalon_request (16'd0, slave_sel, {slave_ovd, 2'b10});
-      avalon_polling (8, n);
-      // expect no response
-      if (data[0] !== 1'b1) begin
-        error = error+1;
-        $display("ERROR: (t=%0t)  Wrong presence detect responce ('1' expected).", $time);
-      end
-
-      // generate a reset pulse
-      slave_ena   = 1'b1;
-      slave_dat_r = 1'b1;
-      avalon_request (16'd0, slave_sel, {slave_ovd, 2'b10});
-      avalon_polling (8, n);
-      // expect presence response
-      if (data[0] !== 1'b0) begin
-        error = error+1;
-        $display("ERROR: (t=%0t)  Wrong presence detect response ('0' expected).", $time);
-      end
-
-      // write '0'
-      slave_ena   = 1'b1;
-      slave_dat_r = 1'b1;
-      avalon_request (16'd0, slave_sel, {slave_ovd, 2'b00});
-      avalon_polling (8, n);
-      // check if '0' was written into the slave
-      if (slave_dat_w[slave_sel] !== 1'b0) begin
-        error = error+1;
-        $display("ERROR: (t=%0t)  Wrong write data for write '0'.", $time);
-      end
-      // check if '0' was read from the slave
-      if (data[0] !== 1'b0) begin
-        error = error+1;
-        $display("ERROR: (t=%0t)  Wrong read  data for write '0'.", $time);
-      end
-
-      // write '1', read '1'
-      slave_ena   = 1'b1;
-      slave_dat_r = 1'b1;
-      avalon_request (16'd0, slave_sel, {slave_ovd, 2'b01});
-      avalon_polling (8, n);
-      // check if '0' was written into the slave
-      if (slave_dat_w[slave_sel] !== 1'b1) begin
-        error = error+1;
-        $display("ERROR: (t=%0t)  Wrong write data for write '1', read '1'.", $time);
-      end
-      // check if '1' was read from the slave
-      if (data[0] !== 1'b1) begin
-        error = error+1;
-        $display("ERROR: (t=%0t)  Wrong read  data for write '1', read '1'.", $time);
-      end
-
-      // write '1', read '0'
-      slave_ena   = 1'b1;
-      slave_dat_r = 1'b0;
-      avalon_request (16'd0, slave_sel, {slave_ovd, 2'b01});
-      avalon_polling (8, n);
-      // check if '0' was written into the slave
-      if (slave_dat_w[slave_sel] !== 1'b0) begin
-        error = error+1;
-        $display("ERROR: (t=%0t)  Wrong write data for write '1', read '0'.", $time);
-      end
-      // check if '0' was read from the slave
-      if (data[0] !== 1'b0) begin
-        error = error+1;
-        $display("ERROR: (t=%0t)  Wrong read  data for write '1', read '0'.", $time);
-      end
-
-    end  // slave_ovd
-
-  end  // slave_sel
-
-  // test power supply on a typical normal mode slave
-  slave_sel = 0;
-
-  // generate a delay pulse (1ms) with power supply enabled
-  avalon_request (16'd1, slave_sel, 3'b011);
-  avalon_polling (1, n);
-  // check if '1' was read from the slave
-  if ((data[0] !== 1'b1) & ~slave_ovd) begin
-    error = error+1;
-    $display("ERROR: (t=%0t)  Wrong presence detect response (power expected).", $time);
-  end
-  // check if power is present
-  if (owr_p[slave_sel] !== 1'b1) begin
-    error = error+1;
-    $display("ERROR: (t=%0t)  Wrong line power state", $time);
-  end
-
-  // generate a idle pulse (0ms) with power supply enabled
-  avalon_request (16'd1, slave_sel, 3'b111);
-  avalon_polling (1, n);
-  // check if power is present
-  if (owr_p[slave_sel] !== 1'b1) begin
-    error = error+1;
-    $display("ERROR: (t=%0t)  Wrong line power state", $time);
-  end
-
-  // generate a delay pulse and break it with an idle pulse, before it finishes
-  avalon_request (16'd0, 4'h0, 3'b011);
-  avalon_request (16'd0, 4'h0, 3'b111);
-
-  // wait a few cycles and finish
-  $finish(); 
+  #1ms;
+  onewire_master.onewire_reset (presence);
+  data_w = 8'h55;
+  onewire_master.onewire_byte  (data_w, data_r);
+  #1ms;
 end
-*/
 
 //////////////////////////////////////////////////////////////////////////////
-// onewire master model
+// onewire master/slave models
 //////////////////////////////////////////////////////////////////////////////
-
-onewire_master #() onewire_master (.owr (owr));
 
 // pullup
 pullup onewire_pullup [OWN-1:0] (owr);
 
-//////////////////////////////////////////////////////////////////////////////
-// onewire slave models
-//////////////////////////////////////////////////////////////////////////////
+// onewire master
+onewire_master #() onewire_master (.owr (owr));
 
 // fast slave device
 onewire_slave onewire_slave [OWN-1:0] (.owr (owr));
