7a441855ddef36af0a522dbece7f95f51fc9d777
Iztok Jeras <iztok.jeras@gmail.com>
2012-05-13 11:21:18 +0200

    added conversion from binary dump to .sr files

--- onewire/verilog/onewire_modelsim.sh
+++ onewire/verilog/onewire_modelsim.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
+MODELSIM="$HOME/altera/11.1/modelsim_ase/linux"
+
 # cleanup first
 rm onewire.out
 rm onewire.vcd
 
-# compile and run Verilog projec using "Icarus Verilog"
-iverilog -o onewire.out -g2009 onewire_tb.sv onewire_master.sv onewire_slave.sv
-vvp onewire.out
+# compile and run Verilog projec using "ModelSim"
+$MODELSIM/vlib work
+$MODELSIM/vlog onewire_tb.sv onewire_master.sv onewire_slave.sv
+$MODELSIM/vsim -c -do "run -all" onewire_tb
 
 # convert generated binary dump into .sr file
 sigrok-cli -i onewire.bin -I binary -p 1=OWR -o onewire.sr
